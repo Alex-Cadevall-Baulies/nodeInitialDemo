e6ba0139404601a8d6ebdde12a7a56bd61d04ba6
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-11-17 09:28:25 +0100

    express set-up

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app)
+
+app.get('/',(requ, res) => {
+    res.send('testing')
+});
+
+server.listen(3000, () => {
+    console.log('listening on http://localhost::3000')
+})
