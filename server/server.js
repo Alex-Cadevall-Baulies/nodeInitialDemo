61c1a2e7dd40e71ffe8c3b868e7e9f01f1c3786c
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-12-22 14:42:20 +0100

    cleaned up sockets and created middleware

--- server/server.js
+++ server/server.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const http = require('http').createServer(app);
-const path = require('path');
+
+const cors = require('cors')
+app.use(cors())
+
+//we setup our socket by passing our http and frontend port
+const io = require("socket.io")(http, {
+  cors: {
+      origins: "`http://localhost:5173/`"
+  }
+});
+
+//we require our socket middleware from where we'll fire the socket.on commands
+const socketHandler = require('./socketHandler')
+//set up a function to pass socket and io with hhtp server to use
+const fireSocket = (socket) => {
+  socketHandler(io, socket)
+}
+//when user connects io fires and calls for fireSocket
+io.on('connection', fireSocket)
 
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
@@ -13,44 +31,5 @@ mongoose.connect(`http://localhost:${PORT}`, () =>{
 },
 e => console.error(e)
 )
-
-const io = require("socket.io")(http, {
-    cors: {
-        origins: "`http://localhost:5173/`"
-    }
-});
-
-const cors = require('cors')
-app.use(cors())
-
-app.get('/', (req, res) => {
-    // we use this method instead of (__dirname + 'desired path) as we need to access client folder
-    res.sendFile(path.join(__dirname, '../client/index.html'));
-});
-
-io.on('connection', (socket) => {
-    console.log(`a user with id ${socket.id} connected`);
-    socket.on('disconnect', () => {
-      console.log(`user with id ${socket.id} disconnected`);
-    });
-    
-    socket.on('sendMessage', (msg, room) => {
-        // each time we receive a message from the frontend we log it into backend console
-        console.log(`user ${socket.id} message: ${msg}`);
-        //we will now emit it to all front end users
-        if(room !== "") {
-        io.to(room).emit('showMessage', msg)
-        console.log(`user ${socket.id} joined: ${room}`)
-        }
-        else {
-        io.emit('showMessage', msg)
-        }
-      });
-
-    socket.on('joinRoom', (room) => {
-        socket.join(room)
-        console.log(`you joined ${room}`)
-    })
-  });
 
 http.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`))
---
+++ server/socketHandler.js
@@ -0,0 +1,33 @@
+//socketHandler is called from server.js and is passed both io and socket
+const socketHandler = (io, socket) => {
+  console.log(`a user with id ${socket.id} connected`);
+
+  socket.on('disconnect', () => {
+    console.log(`user with id ${socket.id} disconnected`);
+  });
+
+  socket.on('sendMessage', (msg, room) => {
+    // each time we receive a message from the frontend we log it into backend console
+    console.log(`user ${socket.id} message: ${msg}`);
+    //we will now emit it to all front end users
+    if (room !== "") {
+      io.to(room).emit('showMessage', msg)
+      console.log(`user ${socket.id} joined: ${room}`)
+    } else {
+      io.emit('showMessage', msg)
+    }
+  });
+
+  socket.on('joinRoom', (room) => {
+    socket.join(room)
+    console.log(`you joined ${room}`)
+  })
+}
+
+/* => {
+    console.log(`a user with id ${socket.id} connected`);
+    socket.on('disconnect', () => {
+      console.log(`user with id ${socket.id} disconnected`);
+    });*/
+
+module.exports = socketHandler
